9dd44792821b4e5fc39e084d04b0efad7e6f0fbe
Julio Rodriguez <juliorodriguez@ncl.com>
2020-06-11 21:01:41 -0400

    netlify cms

--- .eleventy.js
+++ .eleventy.js
@@ -2,12 +2,13 @@ const pluginSass = require("eleventy-plugin-sass");
 
 module.exports = function (eleventyConfig) {
     eleventyConfig
-        .addPassthroughCopy("src/assets/js");
+        .addPassthroughCopy("./assets/js")
+        .addPassthroughCopy("./admin");
     
     return {
         passthroughFileCopy: true,
         markdownTemplateEngine: "njk",
-        templateFormats: ["html", "md", "njk", "eot", "ttf", "woff", "woff2", "svg", "jpg", "png", "css", "svg"],
+        templateFormats: ["html", "md", "njk", "eot", "ttf", "woff", "woff2", "svg", "jpg", "png", "css", "svg", "yml"],
         dir: {
             input: "src",
             output: "_site",
