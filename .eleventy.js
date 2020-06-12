08af486d45372eeb9f60b4ec14986cd1020d10a0
Julio Rodriguez <juliorodriguez@ncl.com>
2020-06-12 09:23:54 -0400

    added js back

--- .eleventy.js
+++ .eleventy.js
@@ -8,7 +8,7 @@ module.exports = function (eleventyConfig) {
     return {
         passthroughFileCopy: true,
         markdownTemplateEngine: "njk",
-        templateFormats: ["html", "md", "njk", "eot", "ttf", "woff", "woff2", "svg", "jpg", "png", "css", "svg", "yml"],
+        templateFormats: ["html", "md", "njk", "eot", "ttf", "woff", "woff2", "svg", "jpg", "png", "css", "svg", "yml", "js"],
         dir: {
             input: "src",
             output: "_site",
